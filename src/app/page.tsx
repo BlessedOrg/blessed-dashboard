8e265e697a61098d0f102136df1324a56fbd7339
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-22 13:39:14 +0200

    fix auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
 import { HomeDashboard } from "../components/homeDashboard/HomeDashboard";
 import { Navigation } from "../components/homeDashboard/navigation/Navigation";
 import { redirect } from "next/navigation";
 import { setCookie } from "cookies-next";
+import { useEffect } from "react";
 
 export default function HomePage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-  if (!!searchParams?.token) {
-    setCookie("accessToken", `${searchParams.token}`);
-    redirect("/");
-  }
+  useEffect(() => {
+    if (!!searchParams?.token) {
+      setCookie("accessToken", `${searchParams.token}`);
+      redirect("/");
+    }
+  }, [searchParams]);
   return (
     <div className="flex w-full flex-col">
       <Navigation />
