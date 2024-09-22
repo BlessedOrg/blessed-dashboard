b68bc9cb3947842e961ee8e88e751507324b0852
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-22 13:47:07 +0200

    fix auth

--- src/components/homeDashboard/navigation/Navigation.tsx
+++ src/components/homeDashboard/navigation/Navigation.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { MobileNavigation } from "./MobileNavigation";
 import { CreateAppModal } from "@/src/components/createAppModal/CreateAppModal";
+import { useEffect } from "react";
+import { setCookie } from "cookies-next";
+import { redirect } from "next/navigation";
 
-export const Navigation = () => {
+export const Navigation = ({ searchParams }) => {
+  useEffect(() => {
+    if (!!searchParams?.token) {
+      setCookie("accessToken", `${searchParams.token}`);
+      redirect("/");
+    }
+  }, [searchParams]);
   return (
     <nav className="flex justify-between w-full py-6 bg-background px-6 sticky top-0 left-0 right-0 z-20">
       <Link href={"/"} className="p-2 pr-4 rounded-full bg-white h-[3.25rem] flex items-center justify-center">
