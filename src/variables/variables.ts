8524354f12cf329c6fee61b6fac22e91a47c43d6
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-10-30 14:31:40 +0100

    dogs url update

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -1,6 +1,6 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const landingPageUrl = process.env.NEXT_PUBLIC_LANDING_PAGE_URL;
 const docsUrl = "https://docs.blessed.fan";
-const apiDogsAppUrl = "https://app.apidog.com/project/667885";
-const apiDogsDocsUrl = "https://docs.blessed.fan/api-10702477";
+const apiDogsAppUrl = "https://docs.blessed.fan/api-11184560";
+const apiDogsDocsUrl = "https://docs.blessed.fan/api-11184560";
 export { apiUrl, landingPageUrl, docsUrl, apiDogsDocsUrl };
